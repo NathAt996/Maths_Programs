db6c3247959c5d539def0971c8d3f8ddc97def89
NathAt996 <n.morgan5196@gmail.com>
2025-11-15 18:29:53 +0000

    Array_Mean_Calculator
    This program asks the user to enter a number of elements to their array. It then asks the user to input double values into this array which then prints the mean value of the array.

---
+++ Array_Mean_Calculator.java
@@ -0,0 +1,39 @@
+import java.util.Scanner;
+
+public class Array_Mean_Calculator {
+
+    public static void main(String[] args) {
+
+        //Create a userInput array that calculates mean
+
+        Scanner userInput = new Scanner (System.in);
+
+
+        System.out.println("How many elements in your array?");
+        int elements = userInput.nextInt();
+
+        double [] myList = new double [elements];
+
+        int sum = 0;
+
+        System.out.println("\nEnter " + myList.length + " values");
+
+        
+        for (int i = 0; i < myList.length; i++) {
+
+            myList[i] = userInput.nextDouble();
+
+            sum += myList[i];
+        }
+
+        System.out.println();
+
+        System.out.println("\nThe mean value of the array is " + mean(sum, elements));
+    }
+
+
+    public static double mean (double s, double amount) {
+
+        return s / amount;
+    }
+}
