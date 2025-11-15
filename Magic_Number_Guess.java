78e3dce2355f58ab0396c8278adc88a4698d1d3a
NathAt996 <n.morgan5196@gmail.com>
2025-11-15 16:21:06 +0000

    Magic_Number_Guess
    This program asks the user to guess a random integer between 0 and 100. The program prompts the user based on how high or low their guess is from the correct answer.

---
+++ Magic_Number_Guess.java
@@ -0,0 +1,47 @@
+import java.util.Scanner;
+
+public class Magic_Number_Guess {
+
+    public static void main(String[] args) {
+        
+        int number = (int)(Math.random() * 101);
+
+        Scanner userInput = new Scanner (System.in);
+
+        System.out.println("Guess a magic number between 0 and 100");
+
+        //initialise guess with a number (that isn't between 0 and 100)
+
+        int guess = -1;
+
+        while (guess != number) {
+
+            System.out.println("\nEnter your guess");
+            guess = userInput.nextInt();
+
+
+            if (guess == number) {
+
+                System.out.println("Correct!");
+            }
+
+
+            else if (guess < number) {
+
+                System.out.println("Too low!");
+            }
+
+
+            else if (guess > number) {
+
+                System.out.println("Too high!");
+            }
+
+
+            else {
+
+                System.out.println("Guess out of range!");
+            }
+        }
+    }
+}
